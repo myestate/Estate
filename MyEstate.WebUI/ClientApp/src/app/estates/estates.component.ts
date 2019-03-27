49c5fad4bc28d6f2cf6a94c60461022128e33834
AndriiMalchevskyi <asmalchevskyi@gmail.com>
2019-03-27 11:50:31 +0200

    Fill estates component, fixed error with image in home page

--- MyEstate.WebUI/ClientApp/src/app/estates/estates.component.ts
+++ MyEstate.WebUI/ClientApp/src/app/estates/estates.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estates.component.css']
 })
 export class EstatesComponent implements OnInit {
-
+  items = [new Advertisement("../../assets/summerhouse.jpg", "Summerhouse", 10000, "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+  new Advertisement("../../assets/brickhouse.jpg", "Brickhouse", 7000, "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+  new Advertisement("../../assets/renovated.jpg", "Renovated", 9000, "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export class Advertisement {
+  imageUrl: string
+  title: string
+  price: number
+  description: string
+  constructor(im, tit, pr, des) {
+    this.imageUrl = im
+    this.title = tit
+    this.price = pr
+    this.description=des
+  }
 }
