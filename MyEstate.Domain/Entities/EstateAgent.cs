56eb445dcce6f02fec2d817155bce05458433d32
bohdan-khudio <bahdan510@gmail.com>
2019-04-07 20:20:11 +0300

    fix EstateAgentAPI

--- MyEstate.Domain/Entities/EstateAgent.cs
+++ MyEstate.Domain/Entities/EstateAgent.cs
@@ -13,6 +13,6 @@ namespace MyEstate.Domain.Entities
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string City { get; set; } 
-        public string PhotoUrl { get; set; } 
+        public ICollection<Photo> Photos { get; set; }
     }
 }
