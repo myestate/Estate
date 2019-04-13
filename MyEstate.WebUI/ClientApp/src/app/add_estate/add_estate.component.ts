66ebce0292b9ce26f3144c58124078f19e79f1c5
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-04-13 12:21:13 +0300

    Connect with server side

--- MyEstate.WebUI/ClientApp/src/app/add_estate/add_estate.component.ts
+++ MyEstate.WebUI/ClientApp/src/app/add_estate/add_estate.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth/auth.service';
 import { AlertifyService } from '../_services/alertify/Alertify.service';
+import { NgForm } from '@angular/forms';
+import { Estate } from '../_models/estate';
+import { AnimationStyleMetadata } from '@angular/animations';
+import { EstateService } from '../_services/estate/estate.service';
 
 @Component({
   selector: 'app-add_estate',
@@ -8,13 +12,25 @@ import { AlertifyService } from '../_services/alertify/Alertify.service';
   styleUrls: ['./add_estate.component.css']
 })
 export class Add_estateComponent implements OnInit {
-  model: any = {};
+  @Output() cancelAdding = new EventEmitter();
+  model: Estate;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private addService: EstateService, private alertify: AlertifyService) {   }
 
   ngOnInit() {
   }
 
   add_estate() {
+    this.addService.addEstate(this.model).subscribe(() => {
+      this.alertify.success('Estate successfully added');
+    }, error => {
+     this.alertify.error(error);
+    });
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelAdding.emit(false);
   }
 }
+
