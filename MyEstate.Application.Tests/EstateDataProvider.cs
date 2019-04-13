1e8eb2f3c6241e915bd4a4a1b2697898813a713c
Nazar Heryliv <nazarheryliv12@gmail.com>
2019-04-13 18:33:52 +0300

    Add the first tests on EstateRepository

---
+++ MyEstate.Application.Tests/EstateDataProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEstate.Application.Tests
+{
+    class EstateDataProvider
+    {
+        public static Dictionary<int, Domain.Entities.Estate> EstatesForChechConteinsInDb()
+        {
+            var dictionary = new Dictionary<int, Domain.Entities.Estate>();
+
+            var estateId1 = new Domain.Entities.Estate()
+            {
+                Id = 1,
+                Country = "Ukraine",
+                City = "Lviv",
+                Title = "First Estate",
+                Description = "Its my first estate in db",
+                Price = 10000,
+                Square = 100,
+                Street = "Shevhenka",
+                Floors = 4,
+                Rooms = 4,
+                Owner = new Domain.Entities.User() { Id = 3 },
+                Created = new DateTime(2000, 01, 01),
+                IsActive = true
+            };
+            
+            dictionary.Add(estateId1.Id, estateId1);
+
+            return dictionary;
+        }
+
+        public static List<Domain.Entities.Estate> EstateWithoutIDToAdd()
+        {
+            var estatewithoutId = new List<Domain.Entities.Estate>();
+
+            var estate1 = new Domain.Entities.Estate()
+            {
+                Country = "Ukraine",
+                City = "Lviv",
+                Title = "Title",
+                Description = "Its my second estate",
+                Price = 10000,
+                Square = 100,
+                Street = "Shevhenka",
+                Floors = 4,
+                Rooms = 4,
+                Owner = new Domain.Entities.User() { Id = 3 },
+                Created = new DateTime(2010, 10, 10),
+                IsActive = true
+            };
+
+            estatewithoutId.Add(estate1);
+
+            return estatewithoutId;
+        }
+    }
+}
---
+++ MyEstate.Application.Tests/EstatesRepositoryTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using MyEstate.Application.Interfaces;
+using NUnit.Framework;
+using Persistence;
+
+namespace MyEstate.Application.Tests
+{
+    class EstatesRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var mockDbContext = new Mock<MyEstateContextFactory>().Object;
+            _estatesRepository = new EstatesRepository(mockDbContext.CreateDbContext(new[] { "myDb" }));
+            _idAddDeleteEstate = 2;
+        }
+
+        [TestCase(1)]
+        public void GetEstate_ById_HappyPath(int id)
+        {
+            // Arrange
+            var actualeEstate = EstateDataProvider.EstatesForChechConteinsInDb()
+                .ContainsKey(id) ? EstateDataProvider.EstatesForChechConteinsInDb()[id] : null;
+            // Act
+            var expectedEstate = _estatesRepository.GetEstate(id);
+
+            // Assert
+            Assert.AreEqual(actualeEstate.Id, expectedEstate.Result.Id);
+            Assert.AreEqual(actualeEstate.City, expectedEstate.Result.City);
+            Assert.AreEqual(actualeEstate.Country, expectedEstate.Result.Country);
+        }
+
+        [Test]
+        public void GetEstates_GetAll_CompareOne_HappyPath()
+        {
+            // Arrange
+            int id = 1;
+            var actualeEstate = EstateDataProvider.EstatesForChechConteinsInDb()
+                .ContainsKey(id) ? EstateDataProvider.EstatesForChechConteinsInDb()[id] : null;
+            // Act
+            IEnumerable<Domain.Entities.Estate> expectedEstates = _estatesRepository.GetEstates().Result;
+
+            var expectedOneEstate = expectedEstates.FirstOrDefault(estate => estate.Id == id);
+            // Assert
+            Assert.IsNotEmpty(expectedEstates);
+
+            Assert.AreEqual(actualeEstate.Title, expectedOneEstate.Title);
+            Assert.AreEqual(actualeEstate.City, expectedOneEstate.City);
+            Assert.AreEqual(actualeEstate.Country, expectedOneEstate.Country);
+            Assert.AreEqual(actualeEstate.Street, expectedOneEstate.Street);
+            Assert.AreEqual(actualeEstate.Floors, expectedOneEstate.Floors);
+            Assert.AreEqual(actualeEstate.Rooms, expectedOneEstate.Rooms);
+            Assert.AreEqual(actualeEstate.Square, expectedOneEstate.Square);
+        }
+
+        [Test]
+        public void SaveAll_DoNothing_ReturnFalse()
+        {
+            // arrange
+            //Act
+            bool saved = _estatesRepository.SaveAll().Result;
+            //Assert
+            Assert.IsFalse(saved);
+        }
+
+        [Test]
+        public void AddEstate_HappyPath()
+        {
+            // arrange
+            IEnumerable<Domain.Entities.Estate> start_estates = _estatesRepository.GetEstates().Result;
+            //Act
+
+            var estate = _estatesRepository.
+                AddEstate(EstateDataProvider.EstateWithoutIDToAdd()[0])
+                .Result;
+            IEnumerable<Domain.Entities.Estate> finish_estates = _estatesRepository.GetEstates().Result;
+            
+            bool conditions = (finish_estates.Count() - start_estates.Count()) == 1;
+            //Assert
+            Assert.IsTrue(conditions);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _estatesRepository = null;
+        }
+
+        private IEstatesRepository _estatesRepository;
+        private int _idAddDeleteEstate;
+    }
+}
