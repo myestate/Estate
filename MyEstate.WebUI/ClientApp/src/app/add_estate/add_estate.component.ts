e0106d0c7a3424f3f69c3505f7e94e192219e890
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-04-14 20:23:52 +0300

    Fixed photos adding

--- MyEstate.WebUI/ClientApp/src/app/add_estate/add_estate.component.ts
+++ MyEstate.WebUI/ClientApp/src/app/add_estate/add_estate.component.ts
@@ -5,6 +5,8 @@ import { NgForm } from '@angular/forms';
 import { Estate } from '../_models/estate';
 import { AnimationStyleMetadata } from '@angular/animations';
 import { EstateService } from '../_services/estate/estate.service';
+import { UserService } from '../_services/user/user.service';
+import { getBaseUrl } from 'src/main';
 
 @Component({
   selector: 'app-add-estate',
@@ -14,6 +16,7 @@ import { EstateService } from '../_services/estate/estate.service';
 export class AddEstateComponent implements OnInit {
   @Output() cancelAdding = new EventEmitter();
   estate: Estate = new Estate();
+  model: any = {};
 
   constructor(private addService: EstateService, private alertify: AlertifyService) {   }
 
@@ -21,13 +24,23 @@ export class AddEstateComponent implements OnInit {
   }
 
   add_estate() {
+    this.estate.isActive = true;
+    this.estate.description = `Region: ${this.model.region}\nCondition: ${this.model.condition}\n
+    Balcony: ${this.model.Balcony}\nWalls: ${this.model.Walls}\nHouse type: ${this.model.type}`;
+    this.estate.street += `, ${this.model.building}`;
     this.addService.addEstate(this.estate).subscribe(() => {
       this.alertify.success('Estate successfully added');
     }, error => {
      this.alertify.error(error);
     });
     console.log(this.estate);
+    console.log(this.estate.photos);
   }
+
+  addPhoto(event) {
+    let target = event.target || event.srcElement;
+    this.estate.photos = target.files;
+}
 
   cancel() {
     this.cancelAdding.emit(false);
