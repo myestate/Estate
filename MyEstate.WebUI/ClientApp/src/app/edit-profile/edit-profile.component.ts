581e7de3146cc082f0f0ba2e90c3572f2f39a770
Nazar Heryliv <nazarheryliv12@gmail.com>
2019-04-15 12:29:43 +0300

    fix bug in user-cabinet.component

--- MyEstate.WebUI/ClientApp/src/app/edit-profile/edit-profile.component.ts
+++ MyEstate.WebUI/ClientApp/src/app/edit-profile/edit-profile.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/user';
 import { UserService } from '../_services/user/user.service';
 import { AlertifyService } from '../_services/alertify/Alertify.service';
-import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
