4449f7dcff86cbc3ea1bf4917399dcb04b7e77ef
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-04-16 18:48:24 +0300

    Add GetMessagesForUser method

--- MyEstate.API/Controllers/MessagesController.cs
+++ MyEstate.API/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using MyEstate.Application.Interfaces;
 using MyEstate.Application.Message.Models;
 using MyEstate.Domain.Entities;
+using Persistence.Helpers;
 
 namespace MyEstate.API.Controllers
 {
@@ -40,6 +42,25 @@ namespace MyEstate.API.Controllers
             }
 
             return Ok(messageFromRepo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery]MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+             return Unauthorized();   
+            }
+
+            messageParams.UserId = userId;
+
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+        
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize, messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+
+            return Ok(messages);
         }
 
         [HttpPost]
--- MyEstate.Application/DatingRepository.cs
+++ MyEstate.Application/DatingRepository.cs
@@ -32,9 +32,28 @@ namespace MyEstate.Application
            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
         }
 
-        public Task<PagedList<Domain.Entities.Message>> GetMessagesForUser()
+        public async Task<PagedList<Domain.Entities.Message>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new System.NotImplementedException();
+            var messages = _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
+            .Include( u => u.Recipient).ThenInclude(p => p.Photos)
+            .AsQueryable();
+
+            switch(messageParams.MessageContainer)
+            {
+                case "Inbox":
+                    messages = messages.Where(u=> u.RecipientId == messageParams.UserId);
+                    break;
+                case "Outbox": 
+                    messages = messages.Where(u=> u.SenderId == messageParams.UserId);
+                    break;
+                default:
+                    messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
+                    break;
+            }
+
+            messages = messages.OrderByDescending(d => d.MessageSent);
+            return await PagedList<Message>.CreateAsync(messages,
+            messageParams.PageNumber, messageParams.PageSize);
         }
 
         public Task<IEnumerable<Domain.Entities.Message>> GetMessageThread(int userId, int recipientId)
