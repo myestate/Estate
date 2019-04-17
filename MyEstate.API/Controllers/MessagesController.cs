22acbe390075a4de064f56875971cf2f29e71501
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-04-17 21:42:16 +0300

    Add MarkAsRead functionality

--- MyEstate.API/Controllers/MessagesController.cs
+++ MyEstate.API/Controllers/MessagesController.cs
@@ -82,7 +82,9 @@ namespace MyEstate.API.Controllers
         public async Task<IActionResult> CreateMessage(int userId, 
             MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
             {
              return Unauthorized();   
             }
@@ -100,14 +102,58 @@ namespace MyEstate.API.Controllers
 
             _repo.Add(message);
 
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
-
-            if (await _repo.SaveAll())
-            {
+            if (await _repo.SaveAll()) {
+                var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
                 return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
             }
 
             throw new Exception("Creating the message faild on save");
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+             return Unauthorized();   
+            }
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if(messageFromRepo.SenderId == userId)
+                messageFromRepo.RecipientDeleted = true;
+
+            if(messageFromRepo.RecipientId == userId)
+                messageFromRepo.RecipientDeleted = true;
+        
+            if(messageFromRepo.SenderDeleted && messagesFromRepo.RecipientDeleted)
+                _repo.Delete(messageFromRepo);
+
+            if(await _repo.SaveAll())
+                returnNoContent();
+
+            throw new Exception("Error deleting the message");
+        }
+
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int userId, int id) 
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+             return Unauthorized();   
+            }
+
+            var message = await _repo.GetMessage(id);
+
+            if(message.RecipientId != userId)
+                return Unauthorized();
+
+            message.isRead = true;
+            messageDateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
         }
     }
 }
