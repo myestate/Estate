c0bd7f6057d09791f16b00ce04462d2e03f14e35
Volodymyr <vova27010@gmail.com>
2019-04-20 13:28:09 +0300

    Add some stuff for deploying

--- MyEstate.API/Startup.cs
+++ MyEstate.API/Startup.cs
@@ -90,7 +90,14 @@ namespace MyEstate.API
             app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new {controller = "Fallback", action = "Index"}
+                ); 
+            });
         }
     }
 }
