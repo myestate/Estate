ffa240afdd72318bb4de10293bfdc54347cac493
bohdan-khudio <bahdan510@gmail.com>
2019-04-21 22:35:37 +0300

    Updating roles

---
+++ MyEstate.API/Controllers/AdminController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MyEstate.Application.RoleEdit;
+using Microsoft.AspNetCore.Identity;
+using MyEstate.Domain.Entities;
+
+namespace MyEstate.API.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+
+    public class AdminController : ControllerBase
+    {
+        private readonly MyEstateContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(MyEstateContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("userWhithRoles")]
+
+        public async Task<IActionResult> GetUserWithRoles()
+        {
+            var userList = await (from user in _context.Users
+                                  orderby user.UserName
+                                  select new
+                                  {
+                                      Id = user.Id,
+                                      UserName = user.UserName,
+                                      Roles = (from userRole in user.UserRoles
+                                               join role in _context.Roles
+                                               on userRole.RoleId
+                                               equals role.Id
+                                               select role.Name).ToList()
+                                  }).ToListAsync();
+
+
+            return Ok(userList);
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("editRole/{userName}")]
+        public async Task<IActionResult> EditRoles(string userName, RoleEditDto roleEditDto)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var selectedRoles = roleEditDto.RoleNames;
+            
+            selectedRoles = selectedRoles ?? new string[] { };
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed to add to roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photosForModeration")]
+        public IActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or moderators can see this");
+        }
+    }
+}
--- MyEstate.API/Startup.cs
+++ MyEstate.API/Startup.cs
@@ -66,6 +66,14 @@ namespace MyEstate.API
                 });
 
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                options.AddPolicy("VipOnle", policy => policy.RequireRole("VIP"));
+            });
+
+
             services.AddMvc(options =>
             {
                 var policy = new AuthorizationPolicyBuilder()
