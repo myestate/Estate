93fbd8fe5d99847a787cd721204429fed41fdbd6
Nazar Heryliv <nazarheryliv12@gmail.com>
2019-04-24 02:56:53 +0300

    Add functionality in usercontroller changepassword and getuser

--- MyEstate.API/Controllers/UsersController.cs
+++ MyEstate.API/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using MyEstate.Application.Interfaces;
 using MyEstate.Application.User.Models;
 using MyEstate.Domain.Entities;
+using System.Text;
 
 namespace MyEstate.API.Controllers
 {
@@ -28,7 +29,6 @@ namespace MyEstate.API.Controllers
         }
 
         [HttpGet]
-
         [Route("/api/users/getusers")]
         public async Task<IActionResult> GetUsers()
         {
@@ -57,11 +57,59 @@ namespace MyEstate.API.Controllers
                 return BadRequest();
             }
 
-            //var userFromRepo = await _repo.GetUser(id);
-            
-            var user = _mapper.Map<User>(userForUpdateDto);
-            await _repo.UpdateUser(user);
-            return Ok(userForUpdateDto);
+            var userFromRepo = await _repo.GetUser(id);
+            userFromRepo.Username = userForUpdateDto.Username;
+            userFromRepo.City = userForUpdateDto.City;
+            userFromRepo.Country = userForUpdateDto.Country;
+            //var user = _mapper.Map<User>(userForUpdateDto);
+            var temp = await _repo.UpdateUser(userFromRepo);
+            if(temp)
+            {
+                return Ok(await _repo.GetUser(id));
+            }else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateUserPassword([FromBody]UserForChangePasswordDto userForChangePassDto)
+        {
+            var re = Request;
+            Microsoft.Extensions.Primitives.StringValues values;
+            bool result = re.Headers.TryGetValue("Authorization", out values);
+            if (result && values.Count > 0)
+            {
+                var token = values.SingleOrDefault();
+                var splitToken = token.Split(' ');
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var jsonToken = tokenHandler.ReadJwtToken(splitToken[1]) as JwtSecurityToken;
+                //Find user by id
+                string id = jsonToken.Payload.ContainsKey("nameid") ? jsonToken.Payload["nameid"].ToString() : null;
+                var userFromRepo = await _repo.GetUser(Convert.ToInt32(id));
+
+                if (VerifyPasswordHash(userForChangePassDto.oldPass, userFromRepo.PasswordHash, userFromRepo.PasswordSalt))
+                {
+                    CreatePasswordHash(userForChangePassDto.newPass, out var passwordHash, out var passwordSalt);
+                    userFromRepo.PasswordHash = passwordHash;
+                    userFromRepo.PasswordSalt = passwordSalt;
+
+                    var temp = await _repo.UpdateUser(userFromRepo);
+                    if(temp)
+                    {
+                        return Ok(await _repo.GetUser(Convert.ToInt32(id)));
+                    }else
+                    {
+                        return BadRequest();
+                    }
+                }else{
+                    return BadRequest();
+                }
+
+            }
+
+            return Unauthorized();
         }
 
         [HttpGet]
@@ -87,6 +135,30 @@ namespace MyEstate.API.Controllers
             }
 
             return Unauthorized();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                if (computedHash.Where((t, i) => t != passwordHash[i]).Any())
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
