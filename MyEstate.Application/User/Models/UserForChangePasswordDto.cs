cdc9543339a739a970f7cfe04b9fdaf57972040f
Nazar Heryliv <nazarheryliv12@gmail.com>
2019-04-24 02:57:34 +0300

    Add dto to change password

---
+++ MyEstate.Application/User/Models/UserForChangePasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEstate.Application.User.Models
+{
+    public class UserForChangePasswordDto
+    {
+        public string oldPass { get; set; }
+
+        public string newPass { get; set; }
+    }
+}
